f44f00bc36bdfc80860896ea503292235a60e86c
praveenkr0017 <praveen.kumar1@inverv.com>
2026-01-05 00:04:59 +0530

    Added CORS configuration

--- api.py
+++ api.py
@@ -3,21 +3,33 @@ import os
 import uuid
 
 from fastapi import (
-    FastAPI, 
-    UploadFile, 
-    File, 
+    FastAPI,
+    UploadFile,
+    File,
     HTTPException
 )
+
 from fastapi.responses import (
-    FileResponse, 
+    FileResponse,
     JSONResponse
 )
-# from .services.employee_processor import process_excel
+
+from fastapi.middleware.cors import CORSMiddleware
+
 from bulk_upload.services.employee_processor import process_excel
 from bulk_upload.db import create_job_entry
 from bulk_upload.config import DB_CRED
 
 app = FastAPI(title="Employee Upload API")
+
+# 🔐 CORS CONFIGURATION
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload-employees")
 async def upload_employees(file: UploadFile = File(...)):
@@ -27,20 +39,17 @@ async def upload_employees(file: UploadFile = File(...)):
     if not file.filename.endswith((".xlsx", ".xls")):
         raise HTTPException(status_code=400, detail="Only Excel files allowed")
 
-    # Save uploaded file temporarily
     with tempfile.NamedTemporaryFile(delete=False, suffix=".xlsx") as tmp:
         tmp.write(await file.read())
         temp_path = tmp.name
 
     try:
-        # 🔹 Step 1: read excel to get total rows
         import polars as pl
         df = pl.read_excel(temp_path)
         total_records = df.height
         print(f"[Check] : Total Records found for insertion : {total_records}")
         del df
 
-         # 🔹 Step 2: insert job row (INITIAL)
         try:
             create_job_entry(
                 job_id,
@@ -52,7 +61,6 @@ async def upload_employees(file: UploadFile = File(...)):
         except Exception as e:
             print(f"[JobIdEntryError] : ❌ Failed to create job_id `{job_id}` entry in DB due to `{e}`")
 
-        #  Process file (job_id passed)
         success, failed_df = process_excel(
             file_path=temp_path,
             job_id=job_id
@@ -67,7 +75,6 @@ async def upload_employees(file: UploadFile = File(...)):
                 }
             )
 
-        # If failed → create Excel
         failed_file = temp_path.replace(".xlsx", "_failed.xlsx")
         failed_df.to_pandas().to_excel(failed_file, index=False)
 
@@ -76,7 +83,7 @@ async def upload_employees(file: UploadFile = File(...)):
             filename="failed_records.xlsx",
             media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
         )
-    
+
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
